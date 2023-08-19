7f8500b8c9f3c25c7d41421bb12069a5a61b6cf4
Oleksandr Baranets <oleksandr.baranets1@gmail.com>
2023-08-19 17:09:34 +0200

    Replaced DWM with awesome window manager; Improved NVIM config; REMOVED slstatus;

---
+++ nvim/lua/post-plugin-basics.lua
@@ -0,0 +1,9 @@
+-- ################# Basic settings dependent on plugins ################ --
+
+-- ================= Visualization ================= --
+
+vim.o.termguicolors = true
+vim.o.background = 'light'
+vim.cmd('colorscheme gruvbox')
+vim.api.nvim_command('let g:gruvbox_terminal_italics=1')
+
